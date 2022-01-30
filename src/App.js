c15e861617e31c6f7ea28de3249a6e23e8375bfe
Aldin Masovic <aldin.masovic@bstelecom.ba>
2022-01-30 15:22:04 +0100

    login

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <div class="registration">
+      <p>
+      Don't have account?
+      </p>
+      <Button variant="contained" href="#contained-buttons">
+        Sign up
+      </Button>
+      </div>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          The Ice Hockey
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+<div class="login">
+        <TextField id="outlined-basic" label="Username" variant="outlined" />
+        <TextField id="outlined-password-input" label="Password" type="password" autoComplete="current-password"/>
+
+<Button variant="contained">Login</Button>
     </div>
+      </header>
+</div>
   );
 }
 
